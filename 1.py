51affcde806adab162d98dec2c1b041849e98c87
Evgeny M. Mitushin <mitushinem@gmail.com>
2024-07-27 13:00:58 +0300

    init

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-###
+def add_everything_up(a, b):
+    try:
+        if isinstance(a and b, int) or isinstance(a and b, float):
+            return a + b
+        else:
+            raise TypeError
+
+    except TypeError as exc:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
